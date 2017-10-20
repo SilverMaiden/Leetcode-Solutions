a2e44d979b6207a886a723ef28cd0a9cf47de6a6
SilverMaiden <h.specials@gmail.com>
2017-10-20 10:54:13 -0700

    update solution for reverse_part2.rb

--- reverse_part2.rb
+++ reverse_part2.rb
@@ -1,17 +1,40 @@
 def reverse_part2(s)
-  #might be easier to split into an array, for each in array, while loop within
-  #while i >= 0, same as before
-  #this time, in the for loop, each time we must reset the value of i to a new .size
-
-  string_list = s.split
-  output = ""
-  for each in string_list
-    i = each.size
-    while i >= 0
-      output += s[i]
-      i -= 1
-    end
+    
+  #Basic test cases, for inputs with only the same letters, and inputs with only one character
+  if s.size <= 1
+      return s
+  end
+      
+  if s.count(s[0]) == s.size
+      return s
+  end
+  
+  #Splitting the string, and including the white space
+  string_list = s.split(/(\W)/)
+  
+  output = []
+  
+  #Setting list_size to the negative value of the size of string_list, so we can reversing at the end of the list
+  list_size = -(string_list.size)
+  
+  j = -1
+  
+  #The while loop with j lets us start at the end of string_list, and move backwards
+  #The while loop with i reverses each individual word
+  
+  while j >= list_size
+      word = string_list[j]
+      string_assemble = ""
+      i = word.size - 1
+      while i >= 0
+          string_assemble += word[i]
+          i -= 1
+      end
+      output.push(string_assemble)
+      j -= 1
   end
 
+  output = output.join
+  
   return output
 end
